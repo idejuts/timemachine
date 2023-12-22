ba1e3c58e94f6d6d401ed148762e69336358bb9b
idejuts <peterhens.petersons@gmail.com>
2023-12-22 13:06:17 +0200

    aa

--- script.js
+++ script.js
@@ -42,36 +42,10 @@ function moveSecondHands() {
     }, 1000);
   }
 
+// submit
 
-  //gallery
 
-  const slideGallery = document.querySelector('.slides');
-const slides = slideGallery.querySelectorAll('div');
-const scrollbarThumb = document.querySelector('.thumb');
-const slideCount = slides.length;
-const slideHeight = 720;
-const marginTop = 16;
-
-const scrollThumb = () => {
-  const index = Math.floor(slideGallery.scrollTop / slideHeight);
-  scrollbarThumb.style.height = `${((index + 1) / slideCount) * slideHeight}px`;
-};
-
-const scrollToElement = el => {
-  const index = parseInt(el.dataset.id, 10);
-  slideGallery.scrollTo(0, index * slideHeight + marginTop);
-};
-
-document.querySelector('.thumbnails').innerHTML += [...slides]
-  .map(
-    (slide, i) => `<img src="${slide.querySelector('img').src}" data-id="${i}">`
-  )
-  .join('');
-
-document.querySelectorAll('.thumbnails img').forEach(el => {
-  el.addEventListener('click', () => scrollToElement(el));
-});
-
-slideGallery.addEventListener('scroll', e => scrollThumb());
-
-scrollThumb();
+  document.querySelector('.submit-email').addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    document.querySelector('.subscription').classList.add('done');
+  });
